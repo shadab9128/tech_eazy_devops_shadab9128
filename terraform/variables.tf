f117a3acae39113fe00cb05eeef5e8ac03e3caba
Ubuntu <ubuntu@ip-172-31-47-0.eu-north-1.compute.internal>
2025-09-21 23:53:54 +0000

    Initial File Uploaded

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "stage" {
+  description = "Deployment stage: Dev or Prod"
+  type        = string
+  default     = "Dev"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "AWS key pair name for SSH access"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repo to clone"
+  type        = string
+  default     = "https://github.com/Trainings-TechEazy/test-repo-for-devops"
+}
+
