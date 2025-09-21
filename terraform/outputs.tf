bc2c43c77c02f60e8ca42fa87c0d30f292093b2f
Ubuntu <ubuntu@ip-172-31-47-0.eu-north-1.compute.internal>
2025-09-21 23:54:21 +0000

    Initial File Uploaded

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app.id
+}
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.app.public_dns
+}
+
