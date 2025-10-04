76d1adda64f631182459be1f3870d074955e3320
Ubuntu <ubuntu@ip-172-31-47-0.eu-north-1.compute.internal>
2025-10-04 07:13:07 +0000

    assignment-3

---
+++ Assignment-3/terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "alb_dns" {
+  description = "Public ALB DNS"
+  value       = aws_lb.alb.dns_name
+}
+
+output "s3_bucket" {
+  description = "S3 bucket for logs and jars"
+  value       = aws_s3_bucket.logs.bucket
+}
+
+output "alb_logs_bucket" {
+  description = "S3 bucket for ALB access logs"
+  value       = aws_s3_bucket.alb_logs.bucket
+}
+
+#output "asg_name" {
+ # description = "AutoScaling Group name"
+  #value       = aws_autoscaling_group.asg.name
+#}
+
