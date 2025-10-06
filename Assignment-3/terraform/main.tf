6a89f9458814297514d437848ffbd20bac64861a
MD SHADAB ALAM <69630723+shadab9128@users.noreply.github.com>
2025-10-06 22:26:40 +0530

    assignment-3

--- Assignment-3/terraform/main.tf
+++ Assignment-3/terraform/main.tf
@@ -44,9 +44,11 @@ resource "aws_security_group" "sg" {
 }
 
 # Default VPC & Subnets
-data "aws_vpc" "default" { default = true }
 data "aws_subnets" "default" {
-  filter { name = "vpc-id", values = [data.aws_vpc.default.id] }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 # S3 bucket for logs
