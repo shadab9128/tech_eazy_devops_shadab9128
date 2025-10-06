e523b7e123d1e7f7e4749cb088acce32e9e57a2f
MD SHADAB ALAM <69630723+shadab9128@users.noreply.github.com>
2025-10-06 23:04:22 +0530

    assignment-3

--- Assignment-3/terraform/variables.tf
+++ Assignment-3/terraform/variables.tf
@@ -12,4 +12,10 @@ variable "ami_id" {
   type        = string
   default     = "ami-0a716d3f3b16d290c"
 }
+variable "stage" {
+  type        = string
+  description = "Deployment stage"
+  default     = "dev"
+}
+
 
