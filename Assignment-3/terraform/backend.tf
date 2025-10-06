82ca7a202bcd6d00aaf798ccb8f73b33f53278e9
MD SHADAB ALAM <69630723+shadab9128@users.noreply.github.com>
2025-10-06 22:40:09 +0530

    assignment-3

--- Assignment-3/terraform/backend.tf
+++ Assignment-3/terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "techeazy-terraform-state"
+    bucket         = "techeazy-terraform-state-shad"
     key            = "assignment-3/terraform.tfstate"
     region         = "eu-north-1"
-    dynamodb_table = "techeazy-terraform-locks"
     encrypt        = true
+    use_lockfile  = true
   }
 }
 
