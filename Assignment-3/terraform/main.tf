e79be08affc066ed681eb67c66d805fdc2e0eb4e
MD SHADAB ALAM <69630723+shadab9128@users.noreply.github.com>
2025-10-11 18:29:04 +0530

    deploy for assignment-3

--- Assignment-3/terraform/main.tf
+++ Assignment-3/terraform/main.tf
@@ -212,9 +212,19 @@ resource "aws_lb_target_group" "tg" {
 # -----------------------------
 # ALB Listener
 # -----------------------------
-resource "aws_lb_listener" "http_listener" {
+resource "aws_lb_listener" "listener_8080" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+resource "aws_lb_listener" "listener_80" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
