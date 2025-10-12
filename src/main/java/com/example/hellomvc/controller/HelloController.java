916886166980d0e50bcbfc0bbd0d5faf06da8262
shadab hashmi <shadabhashmi@shadabs-MacBook-Air.local>
2025-10-13 01:51:56 +0530

    New Changes

--- src/main/java/com/example/hellomvc/controller/HelloController.java
+++ src/main/java/com/example/hellomvc/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(Model model) {
-        model.addAttribute("message", "Hello from Spring MVC! edited by Shadab");
+        model.addAttribute("message", "Hello from Spring MVC! edited by Shad");
         return "hello";
     }
 }
