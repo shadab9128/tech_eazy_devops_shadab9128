45da9b72b34f6533795f1910ddb2379926dcfaf8
shadab hashmi <shadabhashmi@shadabs-MacBook-Air.local>
2025-10-22 23:14:27 +0530

    new changes

--- Assignment-3/terraform/backend.tf
+++ Assignment-3/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "techeazy-terraform-state-shad"
+    bucket         = "techeazy-terraform-state-shad-1"
     key            = "assignment-3/terraform.tfstate"
     region         = "eu-north-1"
     encrypt        = true
